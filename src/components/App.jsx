03281b9ea79c028e6edad7b881902b5b6bb941c7
belkaur28 <BellamyJTK00@gmail.com>
2024-08-12 00:13:20 +0800

    Implemented a single category filter

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
 
-/*Defining the App functional component */
+/* Task 3: Defining the CategoryFilter functional component 
+
+component assigned to 3 variables:
+1. categories: array of categories
+2. selectedCategory: selected category at that point (indicating the selected value in the dropdown)
+3. onCategoryChange: a callback function that will be called when user selects a different category from the dropdown
+*/
+
+const CategoryFilter = ({ categories, selectedCategory, onCategoryChange }) => {
+  return (
+    // creating dropdown menu with current selected category reflected 
+    // onChange is used here as an event handler for when a user selects a different category
+    <select value={selectedCategory} onChange={(e) => onCategoryChange(e.target.value)}>
+      <option value="">All Categories</option>
+      {categories.map((category) => (
+        <option key={category} value={category}>
+          {category}
+        </option>
+      ))}
+    </select>
+  );
+};
+
+/* Task 1: Defining the App functional component */
 const App = () => {
   // initializing the state 'posts' as an empty array to hold the data
   const [posts, setPosts] = React.useState([]);
+  const [selectedCategory, setSelectedCategory] = React.useState('');
 
   /*useEffect hook is used here to fetch the data from the '/api/posts' */
   React.useEffect(() => {
@@ -13,13 +37,21 @@ const App = () => {
       .then((data) => {setPosts(data.posts);}) // // Update the 'posts' state with the fetched data
   }, []); // Empty dependency array to ensure the effect runs only once after the initial render
 
+  // Extracting unique categories from posts
+  const categories = [...new Set(posts.map((post) => post.category))];
+
   // Rendering the list of posts
   return (
     <div>
       <h1>Posts</h1>
+      <CategoryFilter
+        categories={categories}
+        selectedCategory={selectedCategory}
+        onCategoryChange={setSelectedCategory}
+      /> 
       <ul>
         {posts.map((post) => (
-          // Display each posts's title, content and categories as a list item, using their ID as a unique key
+          // Task 2 : Display each posts's title, content and categories as a list item, using their ID as a unique key
           <li key={post.id}>
             <h2>{post.title}</h2>
             <p>{post.content}</p>
