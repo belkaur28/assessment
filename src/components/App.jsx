9757d89110a96079ad0f9d0a9d6aed13582b6541
belkaur28 <BellamyJTK00@gmail.com>
2024-08-13 15:40:46 +0800

    Implemented load more pagination feature

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,7 +7,6 @@ component assigned to 3 variables:
 2. selectedCategory: selected category at that point (indicating the selected value in the dropdown)
 3. onCategoryChange: a callback function that will be called when user selects a different category from the dropdown
 */
-
 const CategoryFilter = ({ categories, selectedCategory, onCategoryChange }) => {
   return (
     // creating dropdown menu with current selected category reflected 
@@ -28,6 +27,7 @@ const App = () => {
   // initializing the state 'posts' as an empty array to hold the data
   const [posts, setPosts] = React.useState([]);
   const [selectedCategory, setSelectedCategory] = React.useState('');
+  const [visiblePosts, setVisiblePosts] = React.useState(5); // number of posts initially displayed
 
   /*useEffect hook is used here to fetch the data from the '/api/posts' */
   React.useEffect(() => {
@@ -40,6 +40,27 @@ const App = () => {
   // Extracting unique categories from posts
   const categories = [...new Set(posts.map((post) => post.category))];
 
+  /* Task 4: Implementing load more pagination */
+  // Handler to load more posts
+  const handleLoadMore = () => {
+    setVisiblePosts((prevVisiblePosts) => prevVisiblePosts + 5); // Loading 5 more posts everytime we click
+  };
+
+  // function to filter posts based on the selected category
+  const getFilteredPosts = () => {
+    if (selectedCategory) {
+      return posts.filter((post) => post.category === selectedCategory);
+    } else {
+      return posts;
+    }
+  };
+
+  const filteredPosts = getFilteredPosts();
+
+  // Slicing the posts based on visible posts count
+  const currentPosts = filteredPosts.slice(0, visiblePosts);
+
+
   // Rendering the list of posts
   return (
     <div>
@@ -50,7 +71,7 @@ const App = () => {
         onCategoryChange={setSelectedCategory}
       /> 
       <ul>
-        {posts.map((post) => (
+        {currentPosts.map((post) => (
           // Task 2 : Display each posts's title, content and categories as a list item, using their ID as a unique key
           <li key={post.id}>
             <h2>{post.title}</h2>
@@ -59,8 +80,13 @@ const App = () => {
           </li>
         ))}
       </ul>
+      {/* creating the load more button */}
+      {visiblePosts < filteredPosts.length && (
+        <button onClick={handleLoadMore}>Load More</button>
+      )} 
     </div>
   );
 };
+
 
 export default App;
